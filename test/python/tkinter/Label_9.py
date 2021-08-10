81b71309057e03c4f210b64a13745e2221302f3e
Tony Huang <tony1966@ms5.hinet.net>
2021-08-10 15:23:23 +0800

    Add files via upload

---
+++ test/python/tkinter/Label_9.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+
+win=tk.Tk()                                                      
+win.title("tkinter GUI 測試")                           
+win.geometry("500x400")
+lenna=tk.PhotoImage(file="200x200-Lenna.png")
+print(type(lenna))
+msg="""影像處理教科書上著名的 Lenna 圖，取自於 1972 年 Playboy 雜誌上
+模特兒 Lenna Söderberg 的照片。"""
+label1=ttk.Label(win, text=msg, justify="left", image=lenna, compound="top")
+label1.config(relief="solid", padding=10, wraplength=200)
+label1.grid(row=0, column=0)
+label2=ttk.Label(win, text=msg, justify="left", image=lenna, compound="bottom")
+label2.config(relief="solid", padding=10, wraplength=200)
+label2.grid(row=0, column=1)
+win.mainloop()
+
