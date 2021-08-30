a858182f79faa8589c112ef58a11a5e467332630
Tony Huang <tony1966@ms5.hinet.net>
2021-08-30 10:48:47 +0800

    Add files via upload

---
+++ test/python/tkinter/Entry_calculator_ttk.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import ttk
+
+def add():
+    c.set(a.get() + b.get())
+def sub():
+    c.set(a.get() - b.get())
+def mul():
+    c.set(a.get() * b.get())
+def div():
+    if b.get() != 0:
+        c.set(a.get() / b.get())
+    else:
+        c.set(float("inf"))
+
+win=tk.Tk()                                                      
+win.title("tkinter GUI 測試")                           
+win.geometry("500x200")
+a=tk.DoubleVar()
+b=tk.DoubleVar()
+c=tk.DoubleVar()
+ttk.Entry(win, width=10, textvariable=a).grid(row=0, column=0)
+ttk.Label(text="+", width=5).grid(row=0, column=1)
+ttk.Entry(win, width=10, textvariable=b).grid(row=0, column=2)
+ttk.Button(text="=", width=5, command=add).grid(row=0, column=3)
+ttk.Entry(win, width=10, textvariable=c).grid(row=0, column=4)
+ttk.Label(text="-", width=5).grid(row=1, column=1)
+ttk.Button(text="=", width=5, command=sub).grid(row=1, column=3)
+ttk.Label(text="*", width=5).grid(row=2, column=1)
+ttk.Button(text="=", width=5, command=mul).grid(row=2, column=3)
+ttk.Label(text="/", width=5).grid(row=3, column=1)
+ttk.Button(text="=", width=5, command=div).grid(row=3, column=3)
+win.mainloop()
+
