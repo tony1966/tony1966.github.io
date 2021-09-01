9f1ef7714ca0fb1f80e5027a076347c0506e729e
Tony Huang <tony1966@ms5.hinet.net>
2021-09-01 16:52:54 +0800

    Add files via upload

---
+++ test/python/tkinter/Text_with_Scrollbar_2.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from tkinter import ttk
+
+win=tk.Tk()                                                      
+win.title("tkinter GUI 測試")                           
+win.geometry("500x300")
+scrollbar=tk.Scrollbar(win)
+scrollbar.pack(side=tk.LEFT, fill=tk.Y)
+text=tk.Text(win, height=10, bg="aqua")
+text.config(padx=5, pady=5, font="Helvetic 12")
+text.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=text.yview)
+str="Hello World!\n你是在說哈囉嗎?"
+strlist=[]
+for i in range(10):
+    strlist.append(str)
+text.insert("insert", "\n".join(strlist))
+text.pack(side=tk.RIGHT, fill=tk.Y)
+win.mainloop()
