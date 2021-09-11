4a73dca3ff99425385604c074c282c8f196c9465
Tony Huang <tony1966@ms5.hinet.net>
2021-09-12 07:27:16 +0800

    Add files via upload

--- test/python/tkinter/Scale_6.py
+++ test/python/tkinter/Scale_6.py
@@ -10,22 +10,18 @@ win=tk.Tk()
 win.title("tkinter GUI 測試")
 win.geometry("300x250")
 
-var_tk=tk.IntVar()
-var_tk.set(20)
 scale_tk=tk.Scale(win,
-                  variable=var_tk,
+                  from_=-10.0,
+                  to=10.0,
                   command=show_value_tk)
 scale_tk.pack()
 label_tk=tk.StringVar()
 tk.Label(win, textvariable=label_tk).pack()
-
-var_ttk=tk.DoubleVar()
-var_ttk.set(0.7071)
 scale_ttk=ttk.Scale(win,
-                    variable=var_ttk,
+                    from_=-10,
+                    to=10,
                     command=show_value_ttk)
 scale_ttk.pack()
 label_ttk=tk.StringVar()
-label_ttk.set(0.7071)
 tk.Label(win, textvariable=label_ttk).pack()
 win.mainloop()
