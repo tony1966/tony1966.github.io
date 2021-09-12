693ffd080efedf45b4617e29f73ac962e6caa43c
Tony Huang <tony1966@ms5.hinet.net>
2021-09-12 17:10:59 +0800

    Add files via upload

--- test/python/tkinter/Scale_9.py
+++ test/python/tkinter/Scale_9.py
@@ -1,25 +1,22 @@
 import tkinter as tk
 from tkinter import ttk
 
+def show_value_v(value):    
+    label_v.config(text=value)
 def show_value_h(value):    
     label_h.config(text=value)
-def show_value_v(value):    
-    label_v.config(text=value)  
 
 win=tk.Tk()                                                      
 win.title("tkinter GUI 測試")
 win.geometry("300x250")
 
-scale_h=ttk.Scale(win, length=250)
-scale_h["command"]=show_value_h
-scale_h.pack()
-label_h=ttk.Label(win)
-label_h.pack()
-
-scale_v=ttk.Scale(win, length=150)
-scale_v["orient"]="vertical"
-scale_v.config(command=show_value_v)
+scale_v=tk.Scale(win, showvalue=False, command=show_value_v)
 scale_v.pack()
 label_v=ttk.Label(win)
 label_v.pack()
+
+scale_h=tk.Scale(win, orient="horizontal", command=show_value_h)
+scale_h.pack()
+label_h=tk.Label(win)
+label_h.pack()
 win.mainloop()
