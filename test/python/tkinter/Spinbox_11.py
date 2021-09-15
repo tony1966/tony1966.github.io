a721c9c7be7a36fe31f156a876c60f2d33f73660
Tony Huang <tony1966@ms5.hinet.net>
2021-09-15 21:30:39 +0800

    Add files via upload

---
+++ test/python/tkinter/Spinbox_11.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import ttk
+
+def show_value_tk():
+    label_tk["text"]=spinbox_tk.get()
+def show_value_ttk():
+    label_ttk["text"]=spinbox_ttk.get()    
+
+win=tk.Tk()                                                      
+win.title("tkinter GUI 測試")
+win.geometry("300x200")
+
+spinbox_tk=tk.Spinbox(win, from_=-10, to=10)
+spinbox_tk.pack()
+tk.Button(win, text="確定", command=show_value_tk).pack()
+label_tk=tk.Label(win)
+label_tk.pack()
+spinbox_ttk=ttk.Spinbox(win, from_=-10, to=10, state="readonly")
+spinbox_ttk.pack()
+ttk.Button(win, text="確定", command=show_value_ttk).pack()
+label_ttk=ttk.Label(win)
+label_ttk.pack()
+win.mainloop()
