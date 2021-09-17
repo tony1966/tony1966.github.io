b0d1dcd49da400326c93c20a13b055250b3eee86
Tony Huang <tony1966@ms5.hinet.net>
2021-09-17 11:35:11 +0800

    Add files via upload

--- test/python/tkinter/Spinbox_11.py
+++ test/python/tkinter/Spinbox_11.py
@@ -4,20 +4,22 @@ from tkinter import ttk
 def show_value_tk():
     label_tk["text"]=spinbox_tk.get()
 def show_value_ttk():
-    label_ttk["text"]=spinbox_ttk.get()    
+    label_ttk.config(text=spinbox_ttk.get())
+def move_up_tk():
+    spinbox_tk.invoke("buttonup")
+def move_down_tk():
+    spinbox_tk.invoke("buttondown")
 
 win=tk.Tk()                                                      
 win.title("tkinter GUI 測試")
 win.geometry("300x200")
 
-spinbox_tk=tk.Spinbox(win, from_=-10, to=10)
+values_tk=("岳不群", "左冷禪", "定逸師太", "任盈盈", "令狐沖")
+spinbox_tk=tk.Spinbox(win, from_=-10, to=10, values=values_tk)
+spinbox_tk["command"]=show_value_tk
 spinbox_tk.pack()
-tk.Button(win, text="確定", command=show_value_tk).pack()
+tk.Button(win, text="上", command=move_up_tk).pack()
+tk.Button(win, text="下", command=move_down_tk).pack()
 label_tk=tk.Label(win)
 label_tk.pack()
-spinbox_ttk=ttk.Spinbox(win, from_=-10, to=10, state="readonly")
-spinbox_ttk.pack()
-ttk.Button(win, text="確定", command=show_value_ttk).pack()
-label_ttk=ttk.Label(win)
-label_ttk.pack()
 win.mainloop()
--- test/python/tkinter/Spinbox_12.py
+++ test/python/tkinter/Spinbox_12.py
@@ -4,22 +4,20 @@ from tkinter import ttk
 def show_value_tk():
     label_tk["text"]=spinbox_tk.get()
 def show_value_ttk():
-    label_ttk.config(text=spinbox_ttk.get())
+    label_ttk["text"]=spinbox_ttk.get()    
 
 win=tk.Tk()                                                      
 win.title("tkinter GUI 測試")
 win.geometry("300x200")
 
-values_tk=(1, 3, 5, 7, 9, 11, 13, 15)
-spinbox_tk=tk.Spinbox(win, values=values_tk)
-spinbox_tk["command"]=show_value_tk
+spinbox_tk=tk.Spinbox(win, from_=-10, to=10)
 spinbox_tk.pack()
+tk.Button(win, text="確定", command=show_value_tk).pack()
 label_tk=tk.Label(win)
 label_tk.pack()
-values_ttk=[1.2, 4.3, 8.7, 9.3, 11.1, 13.2, 34.1, 78.9]
-spinbox_ttk=ttk.Spinbox(win, values=values_ttk)
-spinbox_ttk["command"]=show_value_ttk
+spinbox_ttk=ttk.Spinbox(win, from_=-10, to=10, state="readonly")
 spinbox_ttk.pack()
+ttk.Button(win, text="確定", command=show_value_ttk).pack()
 label_ttk=ttk.Label(win)
 label_ttk.pack()
 win.mainloop()
