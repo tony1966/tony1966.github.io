760a8bc94cb119afe1b66379e81c2866bb46b208
Tony Huang <tony1966@ms5.hinet.net>
2021-10-06 23:48:10 +0800

    Add files via upload

--- test/python/machine_learning/relu_function.py
+++ test/python/machine_learning/relu_function.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sigmoid(x):
-    return 1/(1 + np.exp(-x))
+def relu(x):
+    return np.maximum(0, x)
 
-plt.title('Sigmoid Function')
+plt.title('ReLU Function')
 plt.xlabel('X')
 plt.ylabel('Y')
 x=np.arange(-5, 5, 0.1)
-y=sigmoid(x)
+y=relu(x)
 plt.plot(x, y)
 plt.show()
--- test/python/machine_learning/sigmoid_and_step_function.py
+++ test/python/machine_learning/sigmoid_and_step_function.py
@@ -1,13 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def step(x):
+    return np.array(x > 0, dtype=np.int)
+
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
 
-plt.title('Sigmoid Function')
+plt.title('Sigmoid & Step Function')
 plt.xlabel('X')
 plt.ylabel('Y')
 x=np.arange(-5, 5, 0.1)
-y=sigmoid(x)
-plt.plot(x, y)
+y1=sigmoid(x)
+y2=step(x)
+plt.plot(x, y1, 'b-', x, y2, 'r--')
 plt.show()
