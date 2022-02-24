546ef52058758d6a5da1c7b8472767d853fa1c04
Tony Huang <tony1966@ms5.hinet.net>
2022-02-24 16:11:12 +0800

    Add files via upload

---
+++ test/python/Matplotlib/matplotlib_grid_test_3.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams["font.family"]=["Microsoft JhengHei"]
+week=['日','一','二','三','四','五','六']
+temperature=[25.4, 23.7, 28.6, 29.2, 24.8, 22.5, 21.9]
+plt.plot(week, temperature)
+plt.title('溫度變化圖')                      
+plt.xlabel('星期')                                                 
+plt.ylabel('攝氏')
+plt.grid(axis='y')   
+plt.show()
