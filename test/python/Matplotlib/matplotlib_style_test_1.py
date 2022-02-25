a2ce4f33f2cef91154e26fb74a347e27806cf1f2
Tony Huang <tony1966@ms5.hinet.net>
2022-02-25 15:15:12 +0800

    Add files via upload

---
+++ test/python/Matplotlib/matplotlib_style_test_1.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+week=['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+temperature=[25.4, 23.7, 28.6, 29.2, 24.8, 22.5, 21.9]
+plt.plot(week, temperature)
+plt.title('Temperature Change')                      
+plt.xlabel('Weekday')                                                 
+plt.ylabel('Celsius')  
+plt.show()
