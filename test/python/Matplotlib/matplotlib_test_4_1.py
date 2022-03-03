36b084c3fc966e819e0d154ac34296a89267cb89
Tony Huang <tony1966@ms5.hinet.net>
2022-03-03 08:49:16 +0800

    Add files via upload

---
+++ test/python/Matplotlib/matplotlib_test_4_1.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt 
+week=['Sun','Mon','Tue','Wen','Thu','Fri','Sat']        #X 軸
+this_week=[25.4, 23.7, 28.6, 29.2, 24.8, 22.5, 21.9]    #本周氣溫
+last_week=[22.2, 25.2, 26.6, 22.8, 20.1, 24.3, 28.9]    #上周氣溫
+plt.plot(week,this_week,'r--o',week, last_week,'b-s')   #繪製兩組資料
+plt.title('Temperature Comparison')                     #設定圖形標題
+plt.xlabel('Week day')                                  #設定 X 軸標籤
+plt.ylabel('Celsius')                                   #設定 Y 軸標籤
+plt.show()
