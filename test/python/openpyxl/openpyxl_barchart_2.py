e0caa145de157ed760a452205ef04382b54c2004
Tony Huang <tony1966@ms5.hinet.net>
2022-06-26 14:32:53 +0800

    Add files via upload

---
+++ test/python/openpyxl/openpyxl_barchart_2.py
@@ -0,0 +1,21 @@
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+wb=xl.Workbook()
+ws=wb.active
+votes=[['候選人', '得票數'],        
+       ['宋楚瑜', 608590],    
+       ['韓國瑜', 5522119],       
+       ['蔡英文', 8170231]]
+for row in votes:    
+    ws.append(row)
+chart=BarChart()
+chart.title='2020 總統大選得票數'
+chart.x_axis.title='候選人'
+chart.y_axis.title='得票數'
+chart.type='bar'
+x=Reference(ws, min_col=1, max_col=1, min_row=2, max_row=ws.max_row)
+y=Reference(ws, min_col=2, max_col=2, min_row=1, max_row=ws.max_row)  
+chart.add_data(y, titles_from_data=True)
+chart.set_categories(x)
+ws.add_chart(chart, 'C1')
+wb.save('2020_presidential_2.xlsx')
---
+++ test/python/openpyxl/openpyxl_linechart_1.py
@@ -0,0 +1,22 @@
+import openpyxl as xl
+from openpyxl.chart import LineChart, Reference
+wb=xl.Workbook()
+ws=wb.active
+dividend=[['年份', '0056殖利率', '0050殖利率'],        
+          ['2015', 4.33, 3.01],
+          ['2016', 5.67, 1.28],
+          ['2017', 3.78, 6.1],
+          ['2018', 5.62, 7.1],
+          ['2019', 6.7, 7.22]]  
+for row in dividend:    
+    ws.append(row)
+chart=LineChart()
+chart.title='0056 vs 0050 殖利率'
+chart.x_axis.title='ETF'
+chart.y_axis.title='殖利率'
+x=Reference(ws, min_col=1, max_col=1, min_row=2, max_row=ws.max_row)
+y=Reference(ws, min_col=2, max_col=3, min_row=1, max_row=ws.max_row)  
+chart.add_data(y, titles_from_data=True)
+chart.set_categories(x)
+ws.add_chart(chart, 'D1')
+wb.save('ETF_dividend_yield_linechart.xlsx')
