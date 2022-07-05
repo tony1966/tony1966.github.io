d5f97286fbf834aa1fc53d300538d72baa43047a
Tony Huang <tony1966@ms5.hinet.net>
2022-07-05 20:29:03 +0800

    Add files via upload

---
+++ test/python/Matplotlib/matplotlib_table_1_1.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams["font.family"]=["Microsoft JhengHei"] 
+columns=["股票名稱", "股票代號", "收盤價"]
+data=[['元大台灣50', '0050', 111.55],
+      ['元大高股息', '0056', 26.81],
+      ['台積電', '2330', 453.5],
+      ['聯電', '2303', 38.05],
+      ['中華電', '2412', 124]]
+plt.axis('off')
+plt.table(cellText=data, colLabels=columns, loc="center")
+plt.show()
