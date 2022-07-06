5c6eed7fc83ad7c31fedfcbaa4110605e22a2466
Tony Huang <tony1966@ms5.hinet.net>
2022-07-07 07:33:11 +0800

    Add files via upload

---
+++ test/python/Matplotlib/matplotlib_table_1_7.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams["font.family"]=["Microsoft JhengHei"] 
+columns=["股票名稱", "股票代號", "收盤價"]
+rows=range(1, 6)
+data=[['元大台灣50', '0050', 111.55],
+      ['元大高股息', '0056', 26.81],
+      ['台積電', '2330', 453.5],
+      ['聯電', '2303', 38.05],
+      ['中華電', '2412', 124]]
+plt.axis('off')
+colColours=['yellow', '#ffff00', '#FFFF00']    
+cellColours=[['white']*3, ['cyan']*3, ['white']*3, ['white']*3, ['white']*3]    
+plt.table(cellText=data, colLabels=columns, loc="center",
+          rowLabels=rows, rowColours=['yellow']*5, colColours=['yellow']*3,
+          cellColours=cellColours, edges='open')
+plt.show()
---
+++ test/python/Matplotlib/matplotlib_table_2_1.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams["font.family"]=["Microsoft JhengHei"] 
+columns=["股票名稱", "股票代號", "收盤價"]
+data=[['元大台灣50', '0050', 111.55],
+      ['元大高股息', '0056', 26.81],
+      ['台積電', '2330', 453.5],
+      ['聯電', '2303', 38.05],
+      ['中華電', '2412', 124]]
+plt.axis('off')
+table=plt.table(cellText=data, colLabels=columns, loc="center")
+table.auto_set_font_size(False)
+table.set_fontsize(14)
+plt.show()
