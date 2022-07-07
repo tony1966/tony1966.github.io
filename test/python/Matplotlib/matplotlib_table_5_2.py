4fd47872f44e420df317d75860a1a1959f000cc7
Tony Huang <tony1966@ms5.hinet.net>
2022-07-07 14:40:36 +0800

    Add files via upload

---
+++ test/python/Matplotlib/matplotlib_table_5_2.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams["font.family"]=["Microsoft JhengHei"] 
+columns=["股票名稱", "股票代號", "收盤價"]
+data=[['元大台灣50', '0050', 111.55],
+      ['元大高股息', '0056', 26.81],
+      ['台積電', '2330', 453.5],
+      ['聯電', '2303', 38.05],
+      ['中華電', '2412', 124]]
+plt.axis('off')
+table=plt.table(cellText=data, colLabels=columns, loc="center",
+                colColours=['yellow']*3)
+for key, cell in table.get_celld().items():
+    if key[0] != 0:
+        cell.set_facecolor('cyan')
+plt.show()
