f30b315e9b4736c9b3e9a718485a88ca80d0023f
Tony Huang <tony1966@ms5.hinet.net>
2022-07-22 16:28:53 +0800

    Add files via upload

---
+++ test/python/Seaborn/seaborn-lineplot-1-5.py
@@ -0,0 +1,10 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+sns.set()
+df=sns.load_dataset('flights')
+flights_wide=df.pivot("year", "month", "passengers")
+sns.lineplot(data=flights_wide, dashes=False, markers=True)
+plt.ylabel('passengers')
+plt.show()
