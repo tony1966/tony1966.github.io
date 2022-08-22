367526f4b215fec6275957fcb1b0ebafad01daa4
Tony Huang <tony1966@ms5.hinet.net>
2022-08-22 10:44:28 +0800

    Create hellow_world_3.py

---
+++ test/flask/hellow_world_3.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+app.debug=True 
+
+@app.route("/")
+def hello():
+    return "<h1><i>Hello World!<i></h1>"
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080)
