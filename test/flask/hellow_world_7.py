82a9792c2a41826c543d9083223edcb8b7d4e6c0
Tony Huang <tony1966@ms5.hinet.net>
2022-08-22 10:55:21 +0800

    Create hellow_world_7.py

---
+++ test/flask/hellow_world_7.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+app.debug=True
+
+@app.route("/")
+def hello():
+    return "<h1>Hello World!</h1>"
+
+@app.route("/hello/")
+def hello_name():
+    name=request.args.get("name") 
+    surname=request.args.get("surname")     
+    return "<h1>Hello, {} {}</h1>".format(name, surname)
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080)
