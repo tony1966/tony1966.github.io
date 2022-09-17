88721a1b841f5ddf880efc42a58ef6eaff69db32
Tony Huang <tony1966@ms5.hinet.net>
2022-09-17 11:14:56 +0800

    Update plotly-express-scatter-1.py

--- test/python/plotly/plotly-express-scatter-1.py
+++ test/python/plotly/plotly-express-scatter-1.py
@@ -1,7 +1,6 @@
-import pandas as pd
 import plotly.express as px    
 weight=[49, 65, 53, 45, 56, 47, 52, 61] # 體重
 height=[159, 177, 156, 163, 164, 158, 166, 171] # 身高    
-data=pd.DataFrame({'weight': weight, 'height': height})    
+data={'weight': weight, 'height': height}  
 fig=px.scatter(data, x='weight', y='height') 
 fig.show()
