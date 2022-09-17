8bf1d10ec24abc74273bb7ade460e4b8f99a9971
Tony Huang <tony1966@ms5.hinet.net>
2022-09-17 11:29:35 +0800

    Create plotly-scatter-offline-2.py

---
+++ test/python/plotly/plotly-scatter-offline-2.py
@@ -0,0 +1,7 @@
+import plotly   
+import plotly.graph_objs as go   
+weight=[49, 65, 53, 45, 56, 47, 52, 61]  # 體重
+height=[159, 177, 156, 163, 164, 158, 166, 171] # 身高    
+data=go.Scatter(x=weight, y=height, mode='markers') # 建立 Scatter 圖形物件
+layout=go.Layout(title='The Relation of Weight and Height') # 建立 Layout 物件 
+plotly.offline.plot({"data": [data], "layout": layout}) 
