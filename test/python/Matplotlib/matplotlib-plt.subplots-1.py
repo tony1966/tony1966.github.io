2138375e99748aeb77cb2f58908e3bb73418493a
Tony Huang <tony1966@ms5.hinet.net>
2022-09-28 14:54:34 +0800

    Add files via upload

---
+++ test/python/Matplotlib/matplotlib-plt.subplots-1.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt  
+
+fig, ax=plt.subplots()         
+type(ax)    
+ax.plot([7, 1, 2, 3, 8, 4, 5, 3, 6, 1])     
+ax.set_title('Demo of plt.subplots()')     
+plt.show()      
