0a11b5a739aec2bbfb85f5c4d93575be74126d0f
Tony Huang <tony1966@ms5.hinet.net>
2022-09-28 21:50:05 +0800

    Add files via upload

---
+++ test/python/Matplotlib/matplotlib-fig.add_axes-1.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt   
+fig=plt.figure()     
+ax=fig.add_axes([0, 0, 1, 1])               
+type(ax) 
+fig.axes                                                 
+fig.gca()    
+ax.plot([7, 1, 2, 3, 8, 4, 5, 3, 6, 1])        
+plt.show()            
