0528ed4228549a96d3472b7bdb742b37f271079e
Tony Huang <tony1966@ms5.hinet.net>
2022-09-29 22:56:45 +0800

    Add files via upload

--- test/python/Matplotlib/matplotlib_fig_subplots_adjust.py
+++ test/python/Matplotlib/matplotlib_fig_subplots_adjust.py
@@ -1,14 +1,11 @@
-import matplotlib.pyplot as plt  
+import matplotlib.pyplot as plt
 import numpy as np
 
-x=np.linspace(0, 10, 100)     
 fig=plt.figure()
-fig.subplots_adjust(0.2, 0.2)
-ax1=fig.add_subplot(211)       
-ax1.plot(x, np.sin(x))    
-ax1.set_title('Sin function')     
-ax2=fig.add_subplot(212)     
-ax2.plot(x, np.cos(x))            
-ax2.set_title('Cos function')     
-plt.tight_layout() 
-plt.show()      
+ax1=fig.add_subplot(2, 1, 1)
+ax2=fig.add_subplot(212)
+ax1.plot(np.random.randn(10))
+ax2.plot(np.random.randn(10))
+fig.set_size_inches(5, 4)
+fig.set_facecolor('cyan')
+plt.show()
