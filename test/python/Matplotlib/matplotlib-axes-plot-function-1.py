0ac8643e9e34a33da1f8dbb1222f5a49553a7e1f
Tony Huang <tony1966@ms5.hinet.net>
2022-10-01 16:54:32 +0800

    Add files via upload

--- test/python/Matplotlib/matplotlib-axes-plot-function-1.py
+++ test/python/Matplotlib/matplotlib-axes-plot-function-1.py
@@ -1,16 +1,17 @@
->>> import matplotlib.pyplot as plt  
->>> import numpy as np  
->>> fig=plt.figure()   
->>> ax=fig.add_subplot()   
->>> x=np.arange(3)   
->>> ax.plot(x, -x**2, label='-x**2')
->>> ax.plot(x, -x**3, label='-x**3')
->>> ax.plot(x, -2*x, label='-2*x')
->>> ax.plot(x, -2**x, label='-2**x')
->>> ax.set_xlabel('x')
->>> ax.set_ylabel('y = f(x)')
->>> ax.set_title('Plotting of Function')
->>> ax.legend()
->>> ax.grid(True)
->>> ax.text(0.25, -5, "Object Oriented Plotting")
->>> plt.show()
+import matplotlib.pyplot as plt  
+import numpy as np 
+
+fig=plt.figure()   
+ax=fig.add_subplot()   
+x=np.arange(3)   
+ax.plot(x, -x**2, label='-x**2')
+ax.plot(x, -x**3, label='-x**3')
+ax.plot(x, -2*x, label='-2*x')
+ax.plot(x, -2**x, label='-2**x')
+ax.set_xlabel('x')
+ax.set_ylabel('y = f(x)')
+ax.set_title('Plotting of Function')
+ax.legend()
+ax.grid(True)
+ax.text(0.25, -5, "Object Oriented Plotting")
+plt.show()
