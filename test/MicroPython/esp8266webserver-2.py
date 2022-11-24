0a32009299b6e1217b436f8f2d0eafbd61ee0a09
Tony Huang <tony1966@ms5.hinet.net>
2022-11-25 07:27:10 +0800

    Add files via upload

--- test/MicroPython/esp8266webserver-2.py
+++ test/MicroPython/esp8266webserver-2.py
@@ -3,23 +3,8 @@ import xtools
 import config
 
 ip=xtools.connect_wifi_led(config.SSID, config.PASSWORD) 
-
-html="""
-<html>
-<head>
-   <meta charset="utf-8">
-   <meta name="viewport" content="width=device-width,initial-scale=1">
-</head>
-<body>
-   <h1>Hello World!</h1>
-</body>
-</html>"""
-
-def handleRoot(socket, args):    
-    global html
-    ESP8266WebServer.ok(socket, "200", "text/html", html)
-    
+  
 ESP8266WebServer.begin(80)
-ESP8266WebServer.onPath("/", handleRoot)
+ESP8266WebServer.setDocPath("/www") 
 while True:
     ESP8266WebServer.handleClient()
