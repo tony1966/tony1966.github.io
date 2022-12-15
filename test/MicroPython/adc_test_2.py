717ff1d27d2e38aecb211e9a2a20ee1103a0fa8a
Tony Huang <tony1966@ms5.hinet.net>
2022-12-15 09:56:28 +0800

    Delete adc_test_2.py

--- test/MicroPython/adc_test_2.py
+++
@@ -1,9 +0,0 @@
-import machine
-import time
-
-while True:
-    adc=machine.ADC(0)  
-    value=adc.read()          
-    value=0.3*value + 0.7*adc.read();    # 積分濾波
-    print(value, str(round(value*100/1024)) + '%') 
-    time.sleep(1)   
