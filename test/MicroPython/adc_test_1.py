305b910650d405a6b07bdec29f7abb80ac2a377b
Tony Huang <tony1966@ms5.hinet.net>
2022-12-15 09:56:16 +0800

    Delete adc_test_1.py

--- test/MicroPython/adc_test_1.py
+++
@@ -1,8 +0,0 @@
-import machine
-import time
-
-while True:
-    adc=machine.ADC(0)    
-    value=adc.read()           
-    print(value, str(round(value*100/1024)) + '%')     
-    time.sleep(1)                
