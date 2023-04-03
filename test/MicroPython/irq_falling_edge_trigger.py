9e2e1d86f8880d46b4921b422f8237068c188b10
Tony Huang <tony1966@ms5.hinet.net>
2023-04-03 08:18:34 +0800

    Add files via upload

---
+++ test/MicroPython/irq_falling_edge_trigger.py
@@ -0,0 +1,28 @@
+from machine import Pin
+from time import sleep
+
+def irq_handler(pin):
+    global irq_pin
+    global irq_occurred
+    irq_occurred=True
+    irq_pin=pin
+
+irq_occurred=False
+led_pin=Pin(2, Pin.OUT)
+led_pin.value(1) 
+btn_pin=Pin(5, Pin.IN, Pin.PULL_UP)
+btn_pin.irq(trigger=Pin.IRQ_FALLING, handler=irq_handler)
+
+while True:
+    if irq_occurred:
+        print(f'{irq_pin} 發生中斷')
+        led_pin.value(0)
+        sleep(3)
+        led_pin.value(1)
+        irq_occurred=False
+        
+        
+
+    
+    
+    
