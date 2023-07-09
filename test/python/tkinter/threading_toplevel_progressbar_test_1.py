49fdeff303064420c2e3fa400f9c43173364dbd0
Tony Huang <tony1966@ms5.hinet.net>
2023-07-09 23:19:09 +0800

    Add files via upload

---
+++ test/python/tkinter/threading_toplevel_progressbar_test_1.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox as msgbox
+import time
+import threading
+
+def start_running_task(task, msg):
+    def check_thread():   # 檢查執行緒
+        if thread.is_alive():   # 執行緒還沒結束 : 繼續每 100 ms 檢查一次
+            progress_win.after(100, check_thread) # 每 100 ms 檢查執行緒 
+        else:  # 若執行緒已結束關閉進度條視窗            
+            progress_win.destroy()
+            msgbox.showinfo('通知訊息', '執行作業已完成')
+    # 建立 TopLevel 彈出視窗
+    progress_win=tk.Toplevel(root)  
+    progress_win.title('處理進度')
+    progress_win.update_idletasks()  # 強制更新視窗更新, 處理所有事件
+    progress_win.geometry('400x140')  # 將彈出視窗左上角拉到指定座標並設定大小
+    progressbar=ttk.Progressbar(progress_win, mode='indeterminate', length=250)
+    progressbar.pack(padx=3, pady=30)
+    progressbar.start(5)   # 起始進度條並設定速度 (值越小跑越快)
+    label=ttk.Label(progress_win)  # 顯示提示詞用
+    label.config(text=msg, font=('Helvetica', 10, 'bold'))  # 設定字型大小
+    label.pack(padx=3, pady=3)    
+    thread=threading.Thread(target=task)  # 建立執行緒
+    thread.start()    # 啟始執行緒
+    progress_win.after(100, check_thread)  # 每 100 ms 檢查執行緒是否還存活    
+
+def task():
+    time.sleep(10)   # 模擬漫長的執行時間
+
+root=tk.Tk()
+root.title('執行緒測試')
+root.geometry('600x400')
+start_button=ttk.Button(root, text='開始執行', command=lambda: \
+                        start_running_task(task, '執行中請稍候 ...'))
+start_button.pack()
+root.mainloop()
