f4c3d7e24b7271b5aa176498e047497c9cd7319d
Tony Huang <tony1966@ms5.hinet.net>
2025-05-18 13:51:35 +0800

    Add files via upload

---
+++ test/python/streamlit/st-radio-test-3.py
@@ -0,0 +1,9 @@
+# st-radio-test-3.py
+import streamlit as st
+
+label='請選擇最喜歡的水果 : '
+options=['apple', 'grape', 'peach', 'mango']
+fruits={'apple': '蘋果', 'grape': '葡萄', 'peach': '桃子', 'mango': '芒果'}
+selection=st.radio(label, options, format_func=lambda x: fruits[x])
+st.write("你選的是：", selection)
+
