f65fd5f663dda17f9f48350adac7490af079089b
Tony Huang <tony1966@ms5.hinet.net>
2025-06-04 02:28:14 +0800

    Update st-form-test-1.py

--- test/python/streamlit/st-form-test-1.py
+++ test/python/streamlit/st-form-test-1.py
@@ -4,8 +4,9 @@ import streamlit as st
 with st.form(key='login_form'):
     account=st.text_input('帳號：')
     password=st.text_input('密碼：', type='password')
-    if st.form_submit_button('提交', type='primary'):
+    if st.form_submit_button('取得表單輸入', type='primary'):
         st.write(f'帳號：{account}')
         st.write(f'密碼：{password}')     
-
-   
+if st.button('取得表單輸入'):
+    st.write(f'帳號：{account}')
+    st.write(f'密碼：{password}')
