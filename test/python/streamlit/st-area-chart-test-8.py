cbfad9ac0c2f3740213ab43c8f9292721dcc3cae
Tony Huang <tony1966@ms5.hinet.net>
2025-06-09 06:51:54 +0800

    Add files via upload

---
+++ test/python/streamlit/st-area-chart-test-8.py
@@ -0,0 +1,21 @@
+# st-area-chart-test-3.py
+import streamlit as st
+import pandas as pd
+
+# 模擬每月營收資料
+data=pd.DataFrame({
+    '月份': ['1月', '2月', '3月', '4月', '5月'],
+    '師大店營收': [120000, 135000, 95000, 150000, 170000],
+    '內湖店營收': [90000, 102000, 99000, 130000, 160000]
+    })
+st.subheader('今年前五月月營收區域圖')
+st.area_chart(
+    data=data,
+    x='月份',
+    y=['師大店營收', '內湖店營收'],
+    stack='normalize'
+    )
+
+
+
+
